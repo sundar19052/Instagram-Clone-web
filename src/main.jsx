04e103f303b3cb84d60f2a47b54273ec74cd4cae
sundar19052 <sundarsrinivasan1905@gmail.com>
2026-03-05 17:01:09 +0530

    change this

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter ,RouterProvider} from 'react-router-dom'
 import Viewstory from './Viewstory.jsx'
-import Profile from './profile.jsx'
+import Profile from './Profile.jsx'
 const router=createBrowserRouter(
 [
   {
